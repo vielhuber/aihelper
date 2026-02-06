031f0504d1fac6b030d9e0495fd22362d3ea6584
David Vielhuber <david@vielhuber.de>
2026-02-06 11:03:29 +0100

    Replace mcp urls.

--- tests/Test.php
+++ tests/Test.php
@@ -662,7 +662,7 @@ class Test extends \PHPUnit\Framework\TestCase
                     while ($i_url <= $i_cur) {
                         $url = $mcp_servers_all[$i_url - 1];
                         // replace chat id with random number
-                        $url = str_replace('%CHAT_ID%', rand(100000, 999999), $url);
+                        $url = str_replace('[CHAT_ID]', '[' . rand(100000, 999999) . ']', $url);
                         $mcp_servers[] = [
                             'url' => $url,
                             'authorization_token' => $access_token
